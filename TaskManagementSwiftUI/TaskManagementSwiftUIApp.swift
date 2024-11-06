e870ab1e1526ab404d3cdce727f84a6f89797802
Ghlijyan Hakob <hakobghlijyan@gmail.com>
2024-11-06 18:31:03 +0400

    Initial Commit

---
+++ TaskManagementSwiftUI/TaskManagementSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementSwiftUIApp.swift
+//  TaskManagementSwiftUI
+//
+//  Created by Hakob Ghlijyan on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
