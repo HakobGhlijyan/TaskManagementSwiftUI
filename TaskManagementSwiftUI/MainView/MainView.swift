0ed5c686c8dfeb89484e7de44ce14cd6d50a5f0e
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-11-08 14:55:37 +0400

    [Refactor] Add v1.gif rename contentView -> mainView

---
+++ TaskManagementSwiftUI/MainView/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  TaskManagementSwiftUI
+//
+//  Created by Hakob Ghlijyan on 06.11.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+#Preview {
+    MainView()
+}
